a1e9905bf9dc6767b5716bcea5f9fac39e7f6db5
Smirnov-O <ss2316544@gmail.com>
2020-05-22 18:29:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ read -p "Intall tmux config [y/n]: " TMUX_CONF
 read -p "Install zsh config [y/n]: " ZSH_CONF
 
 if [[ "$VIM_CONF" = "y" ]]; then
-	sudo apt-get install vim curl git
+	sudo apt-get install vim curl git -y
 
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	ln vimrc  $HOME_PAHT/.vimrc
@@ -18,15 +18,18 @@ elif [[ "$GIT_CONF" = "y" ]]; then
 	ln gitignore_global $HOME_PAHT/.gitignore_global
 
 elif [[ "$BASH_CONF" = "y" ]]; then
+	sudo apt-get install bash -y
 	ln bashrc $HOME_PAHT/.bashrc
 
 elif [[ "$TMUX_CONF" = "y" ]]; then
+	sudo apt-get install tmux git -y
 	git clone https://github.com/gpakosz/.tmux.git $HOME_PAHT/.tmux
 	mv $HOME_PAHT/.tmux/.tmux.conf $HOME_PAHT/.tmux.conf
 	mv $HOME_PAHT/.tmux/.tmux.conf.local $HOME_PAHT/.tmux.conf.local
 	# rm -rf $HOME_PAHT/.tmux
 
 elif [[ "$ZSH_CONF" = "y" ]]; then
+	sudo apt-get install zsh curl git 
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	
 	echo "alias cls='clear'" >> $HOME_PAHT/.zshrc
