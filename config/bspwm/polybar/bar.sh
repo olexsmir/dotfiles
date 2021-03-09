5cc8e020c192585faa2147c4fb2afc0684da5133
Smirnov Alexandr <ss2316544@gmail.com>
2021-03-09 20:17:44 +0200

    Update bin/giti, polybar(for bspwm), vscode

--- config/bspwm/polybar/bar.sh
+++ config/bspwm/polybar/bar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -c ~/.config/bspwm/polybar/polybar bar&
+polybar -c ~/.config/bspwm/polybar/polybar min&
+#polybar -c ~/.config/bspwm/polybar/polybar bar&
