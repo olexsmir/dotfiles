4937895bfae70f77323bbe9c06886fc025cc85d4
flof-ik <ss2316544@gmail.com>
2022-02-11 11:40:19 +0200

    updated git, zsh, vscode

---
+++ config/fish/config.fish
@@ -0,0 +1,50 @@
+## Variables
+set -g EDITOR nvim
+set -g GOPATH $HOME/go
+set -g PATH $HOME/bin $PATH
+set -g PATH $HOME/go/bin $PATH
+set -g PATH $HOME/.local/bin $PATH
+set -g PATH $HOME/.golang/bin $PATH
+set -g PATH $HOME/.yarn/bin $PATH
+set -g PATH $HOME/.cargo/bin $PATH
+
+## Configure
+set fish_greeting
+if status is-interactive
+    starship init fish | source
+    zoxide init fish | source
+    fnm env | source
+    source $HOME/.asdf/asdf.fish
+end
+
+function fish_user_key_bindings
+  fish_vi_key_bindings
+
+  bind -M insert -m default jk backward-char force-repaint
+end
+
+
+## Aliases
+alias cls clear
+alias cp "cp -r"
+alias mkdir "mkdir -p"
+
+## Editor
+alias vim nvim
+alias vi vim
+alias v vi
+alias e vi
+
+## GO aliases
+alias gor "go run"
+alias gob "go build"
+alias gog "go get"
+
+## Navigations
+alias ... "cd ../.."
+alias .3 "cd ../../.."
+
+## Exa
+alias ls "exa -l"
+alias ll ls
+alias la "ls -a"
