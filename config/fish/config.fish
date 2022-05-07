b14b4d8bbb66ede97bbb64d291f6418f1e24ad96
Smirnov Olexander <ss2316544@gmail.com>
2022-05-08 00:14:26 +0300

    updated colorscheme, path, vscode config
    * updated colorscheme in kitty, alacritty
    * add nix paths in path(fish shell)
    * remove unused options in vscode

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,14 +9,15 @@ set -g PATH $HOME/.golang/bin $PATH
 set -g PATH $HOME/.yarn/bin $PATH
 set -g PATH $HOME/.cargo/bin $PATH
 set -g PATH $HOME/.deno/bin $PATH
+set -g PATH $HOME/.nix-profile/bin $PATH
+
+set -g LOCALE_ARCHIVE /usr/lib/locale/locale-archive
 
 set fish_greeting
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
     fnm env | source
-
-    source ~/.asdf/asdf.fish
 end
 
 function fish_user_key_bindings
