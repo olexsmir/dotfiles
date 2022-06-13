6716ae034472453835b3c78123b7ddbefdec1ac7
Smirnov Olexander <ss2316544@gmail.com>
2022-06-13 22:02:20 +0300

    clean fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,20 +8,15 @@ set -g PATH $HOME/.local/bin $PATH
 set -g PATH $HOME/.golang/bin $PATH
 set -g PATH $HOME/.yarn/bin $PATH
 set -g PATH $HOME/.cargo/bin $PATH
-set -g PATH $HOME/.deno/bin $PATH
-set -g PATH $HOME/.nix-profile/bin $PATH
 
-set -g LOCALE_ARCHIVE /usr/lib/locale/locale-archive
-
-set fish_greeting
 if status is-interactive
-    starship init fish | source
-    zoxide init fish | source
-    fnm env | source
-end
+  set fish_greeting
+  starship init fish | source
+  zoxide init fish | source
+  fnm env | source
 
-function fish_user_key_bindings
-  fish_vi_key_bindings
-
-  bind -M insert -m default jk backward-char force-repaint
+  function fish_user_key_bindings
+    fish_vi_key_bindings
+    bind -M insert -m default jk backward-char force-repaint
+  end
 end
