1e1920ed43f2f0b83cbfb416870daa15c7023863
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-21 14:24:12 +0200

    fish: t for tmux

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -14,9 +14,9 @@ alias .3 "cd ../../.."
 alias killbt "rfkill block bluetooth"
 alias unkillbt "rfkill unblock bluetooth"
 
-alias g git
 if type -q hledger; alias f hledger; end
 if type -q jj; abbr --add j jj; end
+if type -q tmux; alias t "tmux attach 2>/dev/null || tmux"; end
 
 if type -q nvim
   alias vim nvim
