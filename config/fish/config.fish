e124f15fea49448f4be5da58fae769da4ca40058
Oleksandr Smirnov <olexsmir@gmail.com>
2026-04-28 16:37:25 +0300

    jj push; set aliases in config.fish; apparently i use tags not only in root of the project

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,6 +20,10 @@ if type -q nvim
   set -gx EDITOR nvim
   set -gx VISUAL nvim
   set -gx MANPAGER "nvim +Man!"
+
+  alias vim nvim
+  alias e vim
+  alias ee "nvim -S"
 end
 
 if status is-interactive
@@ -29,4 +33,32 @@ if status is-interactive
   function fish_user_key_bindings
     fish_vi_key_bindings
   end
+end
+
+alias cls clear
+alias cp "cp -r"
+alias rm "rm -i"
+alias mkdir "mkdir -p"
+alias free "free -h"
+alias df "df -h"
+alias du "du -h"
+alias chmox "chmod +x"
+alias ":q" exit
+
+alias ... "cd ../.."
+alias .3 "cd ../../.."
+
+if type -q hledger; alias f hledger; end
+if type -q jj; abbr --add j jj; end
+if type -q tmux; alias t "tmux attach 2>/dev/null || tmux"; end
+
+if type -q eza
+  alias ls "eza -l"
+  alias la "ls -a"
+  alias ll ls
+end
+
+if test "$TERM" = "xterm-kitty"
+  alias ssh "kitty +kitten ssh"
+  alias icat "kitty +kitten icat"
 end
