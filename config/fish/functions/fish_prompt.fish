924e5620b7a1f6a6187b406508edc84b062f545f
Oleksandr Smirnov <olexsmir@gmail.com>
2026-08-31 21:32:42 +0300

    add zmx session name to fish prompt; update kitty config

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -6,8 +6,10 @@ function fish_prompt
   set -l close_color normal
   test $last_status -ne 0; and set close_color red
 
+  set -l zmx_segment ''
+  test -n "$ZMX_SESSION"; and set zmx_segment (set_color --dim blue)"$ZMX_SESSION "(set_color normal)
+
   set -l vcs_segment ''
   test -n "$vcs"; and set vcs_segment (printf ' (%s%s%s%s)' (set_color blue) $vcs (set_color $close_color) '')
-
-  printf '%s%s%s%s ' (set_color cyan) $cwd (set_color normal) $vcs_segment(set_color normal)
+  printf '%s%s%s%s%s ' $zmx_segment (set_color cyan) $cwd (set_color normal) $vcs_segment(set_color normal)
 end
